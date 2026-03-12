11fd1ac677f872c887eb12624aeae0c722c6b285
K.Rohith kumar <rohithkumarkolluru48@gmail.com>
2026-03-12 11:43:23 +0530

    Add files via upload

---
+++ transport-springboot-exam/src/main/java/com/klef/fsad/exam/TransportExamApplication.java
@@ -0,0 +1,11 @@
+package com.klef.fsad.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportExamApplication {
+ public static void main(String[] args) {
+  SpringApplication.run(TransportExamApplication.class, args);
+ }
+}
---
+++ transport-springboot-exam/src/main/java/com/klef/fsad/exam/TransportRepository.java
@@ -0,0 +1,5 @@
+package com.klef.fsad.exam;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransportRepository extends JpaRepository<Transport,Integer>{}
